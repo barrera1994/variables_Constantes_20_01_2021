0ec96f1580a995e80dba763c55c557e69b2f03ee
andres julian barrera diaz <ing.julian.26@gmail.com>
2021-01-28 23:36:35 -0500

    variables_Constantes_20_01_2021

---
+++ main.js
@@ -0,0 +1,18 @@
+vaso = 'gaseosa';
+console.log(vaso);
+vaso = 'jugo';
+console.log(vaso);
+
+nombre = 'Julian';
+edad = 27;
+profesor = false;
+informacion_personal = {
+    id: 1090473520,
+    direccion: 'calle 10 #6-12'
+}
+
+console.log(nombre);
+console.log(edad);
+console.log(profesor);
+console.log(informacion_personal);
+
